21e486dd4372d6e8d8ad937845728cfb3466c4d4
葉力瑜 <yehleeyu@yeliyude-MacBook-Air.local>
2022-09-29 10:22:17 +0100

    add calculate avg of lenghth reviews-analystics.py

--- reviews-analystics.py
+++ reviews-analystics.py
@@ -6,3 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count = count + 1
 		if count % 1000 == 0:
 		    print(len(data))
+
+print('檔案讀取完畢 總共有', len(data), '筆留言')
+
+total = 0
+for d in data:
+	total = total + (len(d))
+avg = total/len(data)
+print('檔案讀取完畢 平均留言長度為', avg)
