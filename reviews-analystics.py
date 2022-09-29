67d9894679267c1953f87aac66fb9f82a6f85c3a
葉力瑜 <yehleeyu@yeliyude-MacBook-Air.local>
2022-09-29 10:31:04 +0100

    add filiter reviews-analystics.py

--- reviews-analystics.py
+++ reviews-analystics.py
@@ -14,3 +14,10 @@ for d in data:
 	total = total + (len(d))
 avg = total/len(data)
 print('檔案讀取完畢 平均留言長度為', avg)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new),'筆資料留言長度小於100')
+print(new[0])
